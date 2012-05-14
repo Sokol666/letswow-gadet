093c97a81e8d8fa7b5e2decff4f13dcb44f2083d
Sergey <sokol666@live.ru>
2012-05-15 01:09:22 +0400

    2.0.2

--- Gadget/js/flyout.js
+++
@@ -1,5 +0,0 @@
-System.Gadget.onSettingsClosing = SettingsClosing;
-function checkBar()
-{
- if (System.Gadget.Settings.read("Nick")!="") document.getElementById('bar').src="http://letswow.ru/userbar/"+System.Gadget.Settings.read("Nick")+".png";
-}
